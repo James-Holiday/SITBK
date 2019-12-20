beedc051a89755f195f9d0cedc2e74ddef21ce2f
KaaaPowww <jamesholiday22@gmail.com>
2019-12-20 08:58:16 -0700

    route fix

--- app.py
+++ app.py
@@ -16,12 +16,17 @@ class Reservation(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50), nullable=False)
   email = db.Column(db.String(100), nullable=False, unique=True)
-  phone = db.Column(db.Integer,primary_key=True)
+  phone = db.Column(db.Integer)
+  date = db.Column(db.Integer)
+  event = db.Column(db.String(50), nullable=False)
+  
 
-  def __init__(self, name, email, phone):
+  def __init__(self, name, email, phone, date, event):
     self.name = name
     self.email = email
     self.phone = phone
+    self.date = date
+    self.event = event
 
 @app.route("/reservations/create", methods=["POST"])
 def create_reservation():
@@ -30,27 +35,45 @@ def create_reservation():
     name = post_data.get("name")
     email = post_data.get("email")  
     phone = post_data.get("phone")  
+    date = post_data.get("date")  
+    event = post_data.get("event")  
 
-    record = Reservation(name, email, phone)
+    record = Reservation(name, email, phone, date, event)
 
     db.session.add(record)
-    db.session.commit
+    db.session.commit()
 
-  return jsonify("Reservation sent")
+    return jsonify("Reservation sent")
   return jsonify("Error: request must be sent as JSON")  
 
 @app.route("/reservations/get", methods=["GET"])
 def get_all_reservations():
-  all_reservations = db.session.query(Reservation.id, Reservation.name, Reservation.email, Reservation.phone).all()
-  return jsonify(all_competitors)
+  all_reservations = db.session.query(Reservation.id, Reservation.name, Reservation.email, Reservation.phone, Reservation.date, Reservation.event).all()
+  return jsonify(all_reservations)
 
+# @app.route("/reservations/put", methods=["PUT"])
+# def put_reservation():
+#   if request.content_type == "application/json":
+#   put_data = request.get_json()
+#   text = put_data.get("text")
+
+#   record = db.session.query(Reservation.id, Reservation.name, Reservation.email, Reservation.phone, Reservation.date, Reservation.event).all()
+
+#   record.text = text
+#   db.session.commit()
+
+#   return jsonify("Event Updated")
+#   return jsonify("Error: Request must be sent as JSON") 
 
 @app.route("/reservations/delete", methods=["DELETE"])
 def delete_reservation(level):
   record = db.session.query(Reservation).filter(Reservation.id == id).first()
+
   db.session.delete(record)
   db.session.commit()
+
   return jsonify("Record Deleted")
+  return jsonify("Error: Request must be sent as JSON") 
 
 
 if __name__ == "__main__":
